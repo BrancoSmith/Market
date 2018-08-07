a076d6fdb759fae79ffa13e2350b15e89a4e6515
Marco Aurelio Silva Ramos <marco.ramos@agrario.gov.br>
2018-08-07 13:51:10 -0300

    Angular

---
+++ Angular/curso-angular2/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { PrimeiroComponent } from './primeiro/primeiro.component';
+import { CursosModule } from './cursos/cursos.module';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PrimeiroComponent
+  ],
+  imports: [
+    BrowserModule,
+    CursosModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})     
+export class AppModule { }
